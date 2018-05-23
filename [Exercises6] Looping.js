a316d9b18368a06c42b1a4807244f4c68d434822
Susan Nio <susan.wen88@gmail.com>
2018-05-23 19:43:00 +0700

    Adding comments

--- [Exercises6] Looping.js
+++ [Exercises6] Looping.js
@@ -4,20 +4,24 @@ untuk yang kelipatan khusus TIDAK USAH diprint untuk counternya,
 console log  ONLY kelipatan saja.
 console log untuk counternya di DISABLE aja
 ================================================================
+
+Comment kedua after revision:
+Good job! Untuk elsenya bisa di disable jg sebenarnya, tapi keseluruhan sudah benar.
 */
 
 // 1. Melakukan Looping Menggunakan While
-var number = 1;
-console.log("LOOPING PERTAMA"); //Menampilkan judul
+var number = 2;
+console.log("LOOPING PERTAMA");
 while(number<=20){ //Loop akan terus berjalan selama nilai number di bawah atau sama dengan 20
-    console.log(number + " - I love coding"); // Menampilkan nilai number pada iterasi tertentu
-    number++ //Mengubah nilai number dengan menambahkan 1
+    console.log(number + " - I love coding"); 
+    number+=2;
 }
 console.log("LOOPING KEDUA"); //Menampilkan judul
-number -= 1; //Mengurangi nilai number dengan 1, agar number dimulai dari angka 20
-while(number>0){ //Loop akan terus berjalan selama nilai number di atas 0
-    console.log(number + " - I will become fullstack developer"); // Menampilkan nilai number pada iterasi tertentu
-    number-- //Mengubah nilai number dengan mengurangkan 1
+
+number -=2;
+while(number>0){ 
+    console.log(number + " - I will become fullstack developer");
+    number-=2;
 }
 
 // 2. Melakukan Looping Menggunakan For
@@ -44,24 +48,24 @@ for(var counter = 1; counter <101; counter++){
 for(var counter = 1; counter < 101; counter +=2){
     if(counter%3===0){
         console.log(counter+" KELIPATAN 3");
-    } else {
-        console.log("");
+    // } else {
+    //     console.log("");
     }
 }
 
 for(var counter = 1; counter < 101; counter +=5){
     if(counter%6===0){
         console.log(counter+" KELIPATAN 6");
-    } else {
-        console.log("");
+    // } else {
+    //     console.log("");
     }
 }
 
 for(var counter = 1; counter < 101; counter +=9){
     if(counter%10 === 0){
         console.log(counter+" KELIPATAN 10");
-    } else {
-        console.log("");
+    // } else {
+    //     console.log("");
     }
 }
 
